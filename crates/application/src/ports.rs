408730009578415edee603de7769c9bf8499fd9c
Xing <sky00000kimo@gmail.com>
2025-06-30 23:22:23 +0800

    dry

--- crates/application/src/ports.rs
+++ crates/application/src/ports.rs
@@ -7,22 +7,9 @@ use domain::user::User;
 
 //=== Port：UserRepository ===//
 #[async_trait]
+#[cfg_attr(test, mockall::automock)]
 pub trait UserRepository: Send + Sync {
     async fn find(&self, id: &Uuid) -> Result<User, DomainError>;
     async fn save(&self, user: &User) -> Result<(), DomainError>;
     async fn shutdown(&self); // Added for graceful shutdown
-}
-#[cfg(test)]
-pub mod mock {
-    use super::*;
-    use domain::error::DomainError;
-    use mockall::automock; // Added for mock
-
-    #[automock]
-    #[async_trait::async_trait]
-    pub trait UserRepository: Send + Sync {
-        async fn find(&self, id: &uuid::Uuid) -> Result<User, DomainError>;
-        async fn save(&self, user: &User) -> Result<(), DomainError>;
-        async fn shutdown(&self); // Added for graceful shutdown
-    }
 }
